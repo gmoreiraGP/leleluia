535290c68977f085b1b2d44cd745ffa74eb35855
gmoreiraGP <guilhermemoreira@geekplug.com.br>
2020-09-21 14:13:04 -0300

    remove data

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -2,18 +2,13 @@ require('dotenv').config()
 const database = require('../models')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
+const request = require('request')
 
 const generateToken = require('../config/generateToken')
-//const sender = require('../modules/mailer')
-// const mailer = require('../modules/mailer')
-const mailer = require('../modules/mailer')
-//const mailerRequest = require('../modules/mailerRequest')
-// const forgotPasswordTemplate = require('../resources/mail/forgotPasswordTemplate')
-
-const request = require('request')
 
 const apiKey = process.env.SENDGRID_API_KEY
 const template = process.env.TEMPLATE_ID
+const proxy = process.env.PROXY
 
 class AuthController {
   static async auth(req, res) {
@@ -51,42 +46,10 @@ class AuthController {
         { where: { email } }
       )
 
-      // let data = {
-      //   sender: 'no-reply@geekplug.com.br',
-      //   receiver: email,
-      //   token
-      // }
-      // var emailTemplate = {
-      //   to: email,
-      //   from: 'no-reply@geekplug.com.br',
-      //   subject: 'Hi there',
-      //   text: 'Awesome sauce',
-      //   html: `<b>Awesome sauce ${token}</b>`
-      // }
-
-      // await sender.sendEmail(data)
-
-      // mailer.sendMail(
-      //   {
-      //     to: email,
-      //     from: 'no-reply@geekplug.com.br',
-      //     template: 'auth/forgot',
-      //     context: { token }
-      //   },
-      //   err => {
-      //     if (err) {
-      //       return res.status(400).json(err)
-      //     }
-
-      //     return res.status(200).json('Email enviado')
-      //   }
-      // )
-
       const options = {
         method: 'POST',
         url: 'https://api.sendgrid.com/v3/mail/send',
-        proxy:
-          'http://38464613857:22783195@proxy.policiamilitar.sp.gov.br:3128',
+        proxy,
         headers: {
           'content-type': 'application/json',
           authorization: `Bearer ${apiKey}`
